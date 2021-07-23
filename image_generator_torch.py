02fa75ccefe8a3708a8d7904c8d1cfedac96acca
SaulLu <lucilesaul.com@gmail.com>
2021-07-23 17:29:57 +0200

    add args

--- image_generator_torch.py
+++ image_generator_torch.py
@@ -32,7 +32,8 @@ from stegano import lsb
 import json
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-
+do_lanczos = True
+do_augs = True
 
 def sinc(x):
     return torch.where(x != 0, torch.sin(math.pi * x) / (math.pi * x), x.new_ones([]))
@@ -60,17 +61,18 @@ def resample(input, size, align_corners=True):
 
     input = input.view([n * c, 1, h, w])
 
-    if dh < h:
-        kernel_h = lanczos(ramp(dh / h, 2), 2).to(input.device, input.dtype)
-        pad_h = (kernel_h.shape[0] - 1) // 2
-        input = F.pad(input, (0, 0, pad_h, pad_h), "reflect")
-        input = F.conv2d(input, kernel_h[None, None, :, None])
+    if do_lanczos:
+        if dh < h:
+            kernel_h = lanczos(ramp(dh / h, 2), 2).to(input.device, input.dtype)
+            pad_h = (kernel_h.shape[0] - 1) // 2
+            input = F.pad(input, (0, 0, pad_h, pad_h), "reflect")
+            input = F.conv2d(input, kernel_h[None, None, :, None])
 
-    if dw < w:
-        kernel_w = lanczos(ramp(dw / w, 2), 2).to(input.device, input.dtype)
-        pad_w = (kernel_w.shape[0] - 1) // 2
-        input = F.pad(input, (pad_w, pad_w, 0, 0), "reflect")
-        input = F.conv2d(input, kernel_w[None, None, None, :])
+        if dw < w:
+            kernel_w = lanczos(ramp(dw / w, 2), 2).to(input.device, input.dtype)
+            pad_w = (kernel_w.shape[0] - 1) // 2
+            input = F.pad(input, (pad_w, pad_w, 0, 0), "reflect")
+            input = F.conv2d(input, kernel_w[None, None, None, :])
 
     input = input.view([n, c, h, w])
     return F.interpolate(input, size, mode="bicubic", align_corners=align_corners)
@@ -162,7 +164,10 @@ class MakeCutouts(nn.Module):
             offsety = torch.randint(0, sideY - size + 1, ())
             cutout = input[:, :, offsety : offsety + size, offsetx : offsetx + size]
             cutouts.append(resample(cutout, (self.cut_size, self.cut_size)))
-        batch = self.augs(torch.cat(cutouts, dim=0))
+        if do_augs:
+            batch = self.augs(torch.cat(cutouts, dim=0))
+        else:
+            batch = torch.cat(cutouts, dim=0)
         if self.noise_fac:
             facs = batch.new_empty([self.cutn, 1, 1, 1]).uniform_(0, self.noise_fac)
             batch = batch + facs * torch.randn_like(batch)
@@ -212,7 +217,7 @@ intervalo_imagenes = 1
 imagen_inicial = None
 imagenes_objetivo = None
 seed = -1
-max_iteraciones = 1000
+max_iteraciones = 2000
 input_images = ""
 
 nombres_modelos = {
@@ -275,6 +280,8 @@ args = argparse.Namespace(
     display_freq=intervalo_imagenes,
     seed=seed,
     specificity="all",
+    do_lanczos = do_lanczos,
+    do_augs = do_augs
 )
 
 wandb.init(project="vqgan-clip", config=args, tags=["torch"])
